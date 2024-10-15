c8927f1bf235cfdd7e67e225fa327f49a11ca2a2
Aubrey Frank <10919070@uvu.edu>
2024-10-14 18:16:01 -0600

    Lab 3. Created project, adding rigid body and collision.

---
+++ Introduction to Collider and Rigidbody components/Assets/ApplyForce.cs
@@ -0,0 +1,20 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 500);
+
+        void OnCollisionEnter (Collision collision)
+        {
+            Debug.Log("collision detected with " + collision.gameObject.name) ;
+        }
+        
+    }
+
+}
